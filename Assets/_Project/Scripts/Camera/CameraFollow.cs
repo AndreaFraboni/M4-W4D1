25759a5b9e8b76cd5243a7e81da2235f6b65602c
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2026-01-22 20:15:00 +0100

    Simple Camera Follow

--- Assets/_Project/Scripts/Camera/CameraFollow.cs
+++ Assets/_Project/Scripts/Camera/CameraFollow.cs
@@ -1,23 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private GameObject _target;
+    [SerializeField] private float sideOffset;
 
-    [SerializeField] private Vector3 offset;
-
-    [SerializeField] private float speedMouse;
-    [SerializeField] private float bottomClamp;
-    [SerializeField] private float upClamp;
-
-    private Quaternion rotation;
-
-    private float yaw;
-    private float pitch;
+    private Vector3 offset;
+    private Vector3 finalOffset;
 
     private void Awake()
     {
@@ -36,23 +25,16 @@ public class CameraFollow : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void Start()
     {
-        //if (_target == null) return;
-
-        //yaw += Input.GetAxisRaw("Mouse X");
-        //pitch -= Input.GetAxisRaw("Mouse Y");
-
-        //pitch = Mathf.Clamp(pitch, bottomClamp, upClamp);
-        
-        //rotation = Quaternion.Euler(pitch, yaw, 0f);
-
-        //transform.position = _target.transform.position + rotation * offset;
+        offset = transform.position - _target.transform.position;
+        finalOffset = offset;
+        finalOffset.x += sideOffset;
     }
 
     private void LateUpdate()
     {
-        //transform.LookAt(_target.transform.position + Vector3.up * Time.deltaTime);
+        transform.position = _target.transform.position + finalOffset;            
     }
 
 }
