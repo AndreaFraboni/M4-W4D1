e1088b388411060299350797eb8c3a92b5c13e2c
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2026-01-24 14:55:49 +0100

    cursor active to permit interaction by mouse cursor

--- Assets/_Project/Scripts/Camera/OrbitalCamera.cs
+++ Assets/_Project/Scripts/Camera/OrbitalCamera.cs
@@ -16,8 +16,8 @@ public class OrbitalCamera : MonoBehaviour
 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
     }
 
     private void LateUpdate()
